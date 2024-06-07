055502735469340d7b7a9ca7cbb17596fa39293b
Alexis G <116964181+Alexis-Gontier@users.noreply.github.com>
2024-06-07 08:54:36 +0200

    Update configFirebase.js

--- app/db/configFirebase.js
+++ app/db/configFirebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAwKoY1IGdMgtrF19lq0ya6vMfIP4pEDGc",
-  authDomain: "activity-app-3a0fe.firebaseapp.com",
-  projectId: "activity-app-3a0fe",
-  storageBucket: "activity-app-3a0fe.appspot.com",
-  messagingSenderId: "644387625403",
-  appId: "1:644387625403:web:e52b191471b5eed0414c9e"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 export const app = initializeApp(firebaseConfig);
